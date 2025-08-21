07156d4c6f6f1bb250db422a1ca2350b84a2411f
Komal Singh <komal.singh928944@gmail.com>
2025-08-21 14:56:35 +0530

    UI Updated

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import FieldsetInput from "../components/Fieldsetinput";
+import FieldsetInput from "../components/FieldsetInput";
 
 
 
